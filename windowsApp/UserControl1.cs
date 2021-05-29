1f591c0c3e8f8c6ebe64ffccf8bfd6fff82f8dde
yukiabineko <yuki1980426@gmail.com>
2021-05-29 23:51:55 +0900

    'ユーザーカラム設定'

--- windowsApp/UserControl1.cs
+++ windowsApp/UserControl1.cs
@@ -5,14 +5,46 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 
 namespace windowsApp
 {
     public partial class UserControl1 : UserControl
     {
+        public string usersUrl = "";
+
         public UserControl1()
         {
             InitializeComponent();
+            DataGridViewTextBoxColumn nameColumn = new DataGridViewTextBoxColumn();
+            nameColumn.HeaderText = "会員名";
+
+
+            DataGridViewTextBoxColumn mailColumn = new DataGridViewTextBoxColumn();
+            mailColumn.HeaderText = "メールアドレス";
+
+            DataGridViewTextBoxColumn telColumn = new DataGridViewTextBoxColumn();
+            telColumn.HeaderText = "電話番号";
+
+            DataGridViewButtonColumn editBtn = new DataGridViewButtonColumn();
+            editBtn.UseColumnTextForButtonValue = true;
+            editBtn.Text = "編集";
+
+            DataGridViewButtonColumn deleteBtn = new DataGridViewButtonColumn();
+            deleteBtn.UseColumnTextForButtonValue = true;
+            deleteBtn.Text = "削除";
+
+
+
+            dataGridView1.Columns.Add(nameColumn);
+            dataGridView1.Columns.Add(mailColumn);
+            dataGridView1.Columns.Add(telColumn);
+
+
         }
 
         private void label1_Click(object sender, EventArgs e)
