d31b3560c662412197579c5ff1cb2aaa978b8810
yukiabineko <yuki1980426@gmail.com>
2021-06-07 20:28:22 +0900

    店頭陳列商品データ読み込み

--- windowsApp/Product.Designer.cs
+++ windowsApp/Product.Designer.cs
@@ -29,18 +29,134 @@ namespace windowsApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 183);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(863, 543);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(0, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(863, 47);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "店頭陳列商品管理";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(131, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 55);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "陳列商品登録";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(705, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 55);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "全削除";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(125, 341);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(650, 150);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 71);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(638, 34);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(656, 38);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "しばらくおまちください";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button3.Location = new System.Drawing.Point(13, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 55);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "表示";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Product
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Product";
             this.Size = new System.Drawing.Size(863, 726);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- windowsApp/Product.cs
+++ windowsApp/Product.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace windowsApp
 {
@@ -13,6 +16,93 @@ namespace windowsApp
         public Product()
         {
             InitializeComponent();
+            groupBox1.Visible = false;
+            dataGridView1.AllowUserToAddRows = false;
+            progressBar1.Minimum = 10;
+            progressBar1.Maximum = 100;
+            progressBar1.Value = progressBar1.Minimum;
+
+
+            DataGridViewButtonColumn name = new DataGridViewButtonColumn();
+            name.HeaderText = "商品名";
+            name.Width = 140;
+
+
+            DataGridViewButtonColumn price = new DataGridViewButtonColumn();
+            price.HeaderText = "価格";
+            price.Width = 140;
+
+
+            DataGridViewButtonColumn stock = new DataGridViewButtonColumn();
+            stock.HeaderText = "在庫";
+            stock.Width = 120;
+
+            DataGridViewButtonColumn total = new DataGridViewButtonColumn();
+            total.HeaderText = "加工法";
+            total.Width = 140;
+
+            DataGridViewButtonColumn edit = new DataGridViewButtonColumn();
+            edit.Name = "edit";
+            edit.Text = "編集";
+            edit.DefaultCellStyle.BackColor = Color.Blue;
+            edit.DefaultCellStyle.ForeColor = Color.White;
+            edit.UseColumnTextForButtonValue = true;
+            edit.Width = 140;
+
+
+            DataGridViewButtonColumn delete = new DataGridViewButtonColumn();
+            delete.Name = "delete";
+            delete.Text = "削除";
+            delete.DefaultCellStyle.BackColor = Color.Red;
+            delete.DefaultCellStyle.ForeColor = Color.White;
+            delete.UseColumnTextForButtonValue = true;
+            delete.Width = 140;
+
+
+            dataGridView1.Columns.Add(name);
+            dataGridView1.Columns.Add(price);
+            dataGridView1.Columns.Add(stock);
+            dataGridView1.Columns.Add(total);
+            dataGridView1.Columns.Add(edit);
+            dataGridView1.Columns.Add(delete);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+            for(var i = progressBar1.Minimum; i<progressBar1.Maximum; i++)
+            {
+                progressBar1.Value = i;
+            }
+            string url = "https://uematsu-backend.herokuapp.com/orders";
+            using(WebClient webClient = new WebClient())
+            {
+                webClient.DownloadStringAsync(new Uri(url));
+                webClient.DownloadStringCompleted += (s, o) =>
+                {
+                    string data = o.Result;
+                    JArray array = JArray.Parse(data);
+                    Console.WriteLine(data);
+                    foreach(var arr in array)
+                    {
+                        dataGridView1.Rows.Add(
+                           arr["name"],
+                           arr["price"],
+                           arr["stock"],
+                           arr["process"]
+                       );
+                    };
+                    groupBox1.Visible = false;
+                };
+               
+           
+            };
         }
     }
 }
